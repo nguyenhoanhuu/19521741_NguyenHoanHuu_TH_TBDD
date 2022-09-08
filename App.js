0668bdf75d98af7b0801a73a2da360406117ba62
Nguyen Hoan Huu <nguyenhoanhuu8668@gmail.com>
2022-09-08 11:13:26 +0700

    hoan thanh bai 1

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput,Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [text,setText] = useState('');
+    const [result,setResult] = useState('');
+    return (
+        
+        <View style={styles.container}>
+        <Text>Guiline</Text>
+           
+           <TextInput style={styles.textInput} placeholder="Email"
+           
+             onChangeText={(text) => setText(text)}
+            value={text}
+            ></TextInput>
+            <Button title="Submit" onPress={toggleModal} />
+          
+          <Text>kết quả : {result}</Text>
+           
+        </View>
+    );
+    function toggleModal () {
+        setResult(text)
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textInput: {
+        border: '1px solid #fff',
+        height:100, 
+        borderColor: "red",
+    }
 });
